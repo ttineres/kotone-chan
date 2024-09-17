844482c2cbdfb287b725e807f33573f4803c27c5
ttineres <serenittserenitt@gmail.com>
2024-09-17 12:40:15 +0800

    Add logging

--- kotone/bot.py
+++ kotone/bot.py
@@ -5,10 +5,20 @@
 
 import discord
 from discord.ext import commands
+import logging
 import os
 from dotenv import load_dotenv
+
 from keep_alive import keep_alive
 
+
+# Logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.DEBUG,
+    encoding="utf-8",
+    handlers=[logging.StreamHandler()],
+)
 
 # Retrieve token from environment variable
 load_dotenv()
@@ -16,14 +26,16 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 TEST_GUILD = os.getenv("TEST_GUILD")
 DEBUGGING = os.getenv("DEBUGGING")
 
+# Discord intents
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Load commands once bot is ready
 @bot.event
 async def on_ready():
-    print(f"[KOTONE] Logged in as {bot.user}")
+    logging.info(f"[KOTONE] Logged in as {bot.user}")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="世界一可愛い私"))
 
     ignored_files = ["__init__.py"]
@@ -31,19 +43,19 @@ async def on_ready():
     # Add cogs
     for f in os.listdir("kotone/cogs"):
         if f.endswith(".py") and f not in ignored_files:
-            print(f"[KOTONE] Adding kotone/{f}")
+            logging.info(f"[KOTONE] Adding kotone/{f}")
             await bot.load_extension(f"cogs.{f[:-3]}")
 
     # Add slash commands
     for f in os.listdir("kotone/slash"):
         if f.endswith(".py") and f not in ignored_files:
-            print(f"[KOTONE] Adding kotone/{f}")
+            logging.info(f"[KOTONE] Adding kotone/{f}")
             await bot.load_extension(f"slash.{f[:-3]}")
     
     # Add events
     for f in os.listdir("kotone/events"):
         if f.endswith(".py") and f not in ignored_files:
-            print(f"[KOTONE] Adding kotone/{f}")
+            logging.info(f"[KOTONE] Adding kotone/{f}")
             await bot.load_extension(f"events.{f[:-3]}")
     
     if DEBUGGING == "TRUE":
@@ -51,11 +63,11 @@ async def on_ready():
         guild = discord.Object(id=TEST_GUILD)
         bot.tree.copy_global_to(guild=guild)
         await bot.tree.sync(guild=guild)
-        print(f"[KOTONE] DEBUGGING: Updated commands to personal guild successfully.")
+        logging.info(f"[KOTONE] DEBUGGING: Updated commands to personal guild successfully.")
     else:
         await bot.tree.sync()
     
-    print("[KOTONE] Updated all commands successfully.")
+    logging.info("[KOTONE] Updated all commands successfully.")
 
 def main():
     keep_alive()
