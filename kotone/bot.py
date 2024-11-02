ff809bb8758c73b099bd2906d7443d19b79d371d
ttineres <serenittserenitt@gmail.com>
2024-11-02 08:03:07 +0800

    Remove support for !help

--- kotone/bot.py
+++ kotone/bot.py
@@ -30,7 +30,7 @@ DEBUGGING = os.getenv("DEBUGGING")
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", help_command=None, intents=intents)
 
 # Load commands once bot is ready
 @bot.event
