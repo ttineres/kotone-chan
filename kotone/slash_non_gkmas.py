552508b43658afd764568bffa0a4f4811536be4b
ttineres <serenittserenitt@gmail.com>
2024-11-06 01:01:51 +0800

    Fix typo in /kotone-help

--- kotone/slash_non_gkmas.py
+++ kotone/slash_non_gkmas.py
@@ -29,8 +29,7 @@ class NonGKMas(commands.Cog):
             "* `/hatsuboshi`: 初星学園の公式チャンネルから、楽曲や動画一覧が見れます！\n"
             "* `/goldrush`: 学園アイドルマスター GOLD RUSH の便利リンクを確認できます！\n"
             "* `/kotone-hello`：ことねちゃんが挨拶しますよ！\n"
-            "* `/anchor`：`/anchor help`をご参照ください。\n"
-            "* `/secret`：`/secret help`をご参照ください。"
+            "* その他のコマンドのヘルプは`help`で検索してください。\n"
             "* `!kotone`：とっておきのことねちゃんスタンプを見せちゃいます♪",
             ephemeral=True
         )
