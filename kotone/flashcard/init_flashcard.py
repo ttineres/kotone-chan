6bbecaf6bf6a0d983be00a0fc9abc6621e33f45d
ttineres <serenittserenitt@gmail.com>
2024-11-18 19:16:00 +0800

    Update slash_flashcard to use .yaml for P-drink and P-item flashcards

--- kotone/flashcard/init_flashcard.py
+++ kotone/flashcard/init_flashcard.py
@@ -3,6 +3,8 @@
 #
 
 
+import importlib.resources
+import kotone.flashcard
 import yaml
 from typing import Any, Optional
 
@@ -15,7 +17,7 @@ def _parse_flashcard(raw_flashcard: dict[str, Any]) -> dict[str, str]:
 
         `raw_flashcard` must contain keys `"desc"` and `"content"`.
     """
-    result = {"desc": raw_flashcard.get("desc")}
+    result = {"desc": raw_flashcard["desc"]}
     for item in raw_flashcard["content"]:
         title = _parse_title(item["title"], item.get("is_enhanced"), item.get("cost"), item.get("origin"))
         effect = _parse_effect(item["effect"])
@@ -50,11 +52,35 @@ def _parse_effect(effect_lst: list[str]) -> str:
     return "\n ".join("* " + effect for effect in effect_lst)
 
 
+def _parse_flashcard_freq(
+    raw_flashcard_freq: dict[str, Any],
+    full_flashcard: dict[str, str]
+) -> dict[str, str]:
+    """ Returns a partial flashcard dict from `full_flashcard`
+        containing only items specified by `raw_flashcard_freq`.
+
+        Intended for cutting down the size of flashcards
+        so they only contain frequently-used items.
+    """
+    result = {"desc": raw_flashcard_freq["desc"]}
+    title_list = raw_flashcard_freq["content"]
+    for key in full_flashcard.keys() - {"desc"}:
+        if any(key.startswith(title) for title in title_list):
+            result[key] = full_flashcard[key]
+    return result
+
+
 # Load and parse flashcards from .yaml
-with open("flashcard_p_drinks.yaml", encoding="utf-8") as f:
+resource = importlib.resources.files(kotone.flashcard)
+
+with resource.joinpath("flashcard_p_drinks.yaml").open(encoding="utf-8") as f:
     _raw_p_drinks = yaml.safe_load(f)
 _FLASHCARD_P_DRINKS = _parse_flashcard(_raw_p_drinks)
 
-with open("flashcard_p_items.yaml", encoding="utf-8") as f:
+with resource.joinpath("flashcard_p_items.yaml").open(encoding="utf-8") as f:
     _raw_p_items = yaml.safe_load(f)
 _FLASHCARD_P_ITEMS = _parse_flashcard(_raw_p_items)
+
+with resource.joinpath("flashcard_p_items_freq.yaml").open(encoding="utf-8") as f:
+    _raw_p_items_freq = yaml.safe_load(f)
+_FLASHCARD_P_ITEMS_FREQ = _parse_flashcard_freq(_raw_p_items_freq, _FLASHCARD_P_ITEMS)
--- tests/test_slash_flashcard.py
+++ tests/test_slash_flashcard.py
@@ -5,31 +5,52 @@
 
 import yaml
 
-
-# Test .yaml files
-with open("flashcard_p_drinks.yaml", encoding="utf-8") as f:
-    data = yaml.safe_load(f)
-
-assert isinstance(data, dict)
-assert data.get("desc")
-assert data.get("content")
-for item in data.get("content"):
-    assert item.get("title")
-    assert item.get("effect")
-    assert isinstance(item.get("effect"), list)
+from kotone.flashcard.init_flashcard import (
+    _FLASHCARD_P_DRINKS,
+    _FLASHCARD_P_ITEMS,
+    _FLASHCARD_P_ITEMS_FREQ,
+)
 
 
-with open("flashcard_p_items.yaml", encoding="utf-8") as f:
-    data = yaml.safe_load(f)
+def test_yaml_files():
+    with open("kotone/flashcard/flashcard_p_drinks.yaml", encoding="utf-8") as f:
+        data = yaml.safe_load(f)
 
-assert isinstance(data, dict)
-assert data.get("desc")
-assert data.get("content")
-lst = data.get("content")
-for i in range(len(lst)):
-    item = lst[i]
-    assert item.get("title")
-    if item.get("is_enhanced") is False:
-        assert item.get("title") == lst[i+1].get("title") and lst[i+1].get("is_enhanced")
-    assert item.get("effect")
-    assert isinstance(item.get("effect"), list)
+    assert isinstance(data, dict)
+    assert data.get("desc")
+    assert data.get("content")
+    for item in data.get("content"):
+        assert item.get("title")
+        assert item.get("effect")
+        assert isinstance(item.get("effect"), list)
+
+    with open("kotone/flashcard/flashcard_p_items.yaml", encoding="utf-8") as f:
+        data = yaml.safe_load(f)
+
+    assert isinstance(data, dict)
+    assert data.get("desc")
+    assert data.get("content")
+    lst = data.get("content")
+    for i in range(len(lst)):
+        item = lst[i]
+        assert item.get("title")
+        if item.get("is_enhanced") is False:
+            assert item.get("title") == lst[i+1].get("title") and lst[i+1].get("is_enhanced")
+        assert item.get("effect")
+        assert isinstance(item.get("effect"), list)
+
+    with open("kotone/flashcard/flashcard_p_items_freq.yaml", encoding="utf-8") as f:
+        data = yaml.safe_load(f)
+
+    assert isinstance(data, dict)
+    assert data.get("desc")
+    assert data.get("content")
+
+
+def test_init_flashcard():
+    for flashcard_dict in (
+        _FLASHCARD_P_DRINKS,
+        _FLASHCARD_P_ITEMS,
+        _FLASHCARD_P_ITEMS_FREQ,
+    ):
+        assert all(isinstance(k, str) and isinstance(v, str) for k, v in flashcard_dict.items())
