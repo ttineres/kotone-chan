041fcc6ea17bc3c57c5c1e316d01572745417447
ttineres <serenittserenitt@gmail.com>
2025-01-26 20:24:13 +0800

    Update flashcard 20250122

--- tests/test_slash_flashcard.py
+++ tests/test_slash_flashcard.py
@@ -125,7 +125,7 @@ def test_init_flashcard():
 def test_flashcard_length():
     # Compare flashcard with the expected number of items currently in the game
     exp_num_p_drinks = 26
-    exp_num_p_items = 218
+    exp_num_p_items = 221
     exp_num_skillcards = (246 - 1) * 2 + 1
     if len(_FLASHCARD_P_DRINKS.keys()) - 1 != exp_num_p_drinks:
         warnings.warn(UserWarning(f"Expected { exp_num_p_drinks } p-drinks in flashcard, instead found { len(_FLASHCARD_P_DRINKS.keys())-1 }"))
